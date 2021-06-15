3b08804a3452c444525232da1fc7337d029a3687
trop100 <trop100@naver.com>
2021-06-15 03:57:24 +0000

    들여쓰기 수정

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,16 +1,17 @@
 const passport = require("passport");
 
 exports.isLoggedIn = (req, res, next) => {
-  passport.authenticate("jwt", { session: false }, (err, user) => {
-    if (user) {
-      req.user = user;
-      next();
-    } 
-    else {
-      res.status(403).send("로그인 필요");
-    }
-  })(req, res, next);
+    passport.authenticate("jwt", { session: false }, (err, user) => {
+        if (user) {
+            req.user = user;
+            next();
+        }
+        else {
+            res.status(403).send("로그인 필요");
+        }
+    })(req, res, next);
 };
+
 exports.isAuthor = (req, res, next) => {
     if(req.user.type){
         next();
