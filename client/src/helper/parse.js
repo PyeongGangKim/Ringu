3e4026a2f8e3b9971d95c7a5368e3f16fd3ed6b4
trop100 <trop100@naver.com>
2021-06-20 12:27:33 +0000

    parse helper 추가

--- client/src/helper/parse.js
+++ client/src/helper/parse.js
@@ -1,5 +1,14 @@
 module.exports = {
     numberWithCommas: (x) => {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
+    searchToDict: (search) => {
+        var dict = {}
+        search.substring(1, search.length).split('&').map(s => {
+            var token = s.split('=')
+            dict[token[0]] = token[1]
+        })
+
+        return dict
     }
 }
