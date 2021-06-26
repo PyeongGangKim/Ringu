349f79a99ef7cd7509bc24d4cd97419261f3865a
김평강 <21500185@handong.edu>
2021-06-26 21:45:59 +0900

    feat: favortie_book_statisctics 만들고 적용

---
+++ server/models/favorite_book_statistics.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('favorite_book_statistics', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    book_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'book',
+        key: 'id'
+      }
+    },
+    favorite_person_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    created_date_time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    }
+  }, {
+    sequelize,
+    tableName: 'favorite_book_statistics',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
