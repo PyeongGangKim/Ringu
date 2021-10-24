06cde6597ffd7622c0c5d288960d31dfc420f9cc
trop100 <trop100@naver.com>
2021-10-24 08:04:17 +0000

    매출 내역 확인 api 추가

--- server/routes/api/purchase.js
+++ server/routes/api/purchase.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
+var moment = require("moment");
 
 const {StatusCodes} = require("http-status-codes");
 const { isLoggedIn, isAuthor } = require("../../middlewares/auth");
 
 
-const {account ,book_detail, sequelize, member, purchase, book, review, review_statistics, Sequelize : {Op}} = require("../../models");
+const {account, book_detail, sequelize, member, purchase, book, review, review_statistics, Sequelize : {Op}} = require("../../models");
 const {imageLoad} = require("../../middlewares/third_party/aws.js");
 const {kakaopay} = require("../../config/pay.js");
 const url = require("../../config/url.js");
@@ -294,25 +295,38 @@ router.get('/', isLoggedIn, async (req, res, next) => {// 구매한 리스트 �
         }
     }
     catch(err){
+        console.error(err);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             "error": "server error"
         });
-        console.error(err);
     }
 });
-router.get('/sellingList', isLoggedIn, isAuthor,async (req, res, next) => { //작가 입장에서 판 책들 가져오기
+router.get('/sales', isLoggedIn, isAuthor,async (req, res, next) => { //작가 입장에서 판 책들 가져오기
     var author_id = req.query.author_id;
+    var period = parseInt(req.query.period);
+    var period_cond;
+
+    if(period === 0) {
+        period_cond = moment().subtract('1', 'y').toDate()
+    } else if(period === 1) {
+        period_cond = moment().subtract('6', 'M').toDate()
+    } else if(period === 2) {
+        period_cond = moment().subtract('3', 'M').toDate()
+    } else if(period === 3) {
+        period_cond = moment().subtract('1', 'M').toDate()
+    }
+
     try{
-        const selling_list = await purchase.findAll({
+        const sales = await purchase.findAll({
             attributes: [
-                "id",
-                "created_date_time",
-                "price",
-                [sequelize.literal("book_detail.title"), "title"],
-                [sequelize.literal("member.name"), "buyer_name"],
+                [sequelize.literal("SUM(purchase.price)"), "revenue"],
+                [sequelize.literal("(purchase.created_date_time)"), "date"],
             ],
             where: {
                 status : 1,
+                created_date_time: {
+                    [Op.gte]: period_cond,
+                },
             },
             include : [
                 {
@@ -329,33 +343,81 @@ router.get('/sellingList', isLoggedIn, isAuthor,async (req, res, next) => { //�
                         }
                     ]
                 },
-                {
-                    model : member,
-                    as: "member",
-                    attributes : [],
-                }
+            ],
+            group: [
+                [sequelize.literal("DATE_FORMAT(purchase.created_date_time, '%y%m%d')"), "date"]
+            ],
+            order : [
+                ["created_date_time", "ASC"],
             ],
         });
-        if(selling_list.length == 0){
-            console.log(selling_list);
+
+        if(sales.length == 0){
             res.status(StatusCodes.NO_CONTENT).send("No content");;
         }
         else{
-            console.log(selling_list);
             res.status(StatusCodes.OK).json({
-                selling_list : selling_list,
+                sales : sales,
             });
         }
 
     }
     catch(err){
+        console.error(err);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             "error": "server error"
         });
-        console.error(err);
     }
 });
 
+router.get('/sales/amount/author', isLoggedIn, isAuthor, async(req, res, next) => {
+    var author_id = req.query.author_id;
+
+    try {
+        const amount = await purchase.findAll({
+            attributes: [
+                "remit_status",
+                [sequelize.literal("SUM(purchase.price)"), "amount"],
+            ],
+            where: {
+                status: 1,
+            },
+            include: [
+                {
+                    model : book_detail,
+                    as : "book_detail",
+                    attributes : [],
+                    required: true,
+                    include : [
+                        {
+                            model: book,
+                            as : "book",
+                            where : {
+                                author_id: author_id
+                            },
+                            attributes: [],
+                        }
+                    ]
+                },
+            ],
+            group: [
+                "remit_status"
+            ],
+        });
+        if(amount.length === 0){
+            res.status(StatusCodes.NO_CONTENT).send("No content");;
+        }
+        else{
+            res.status(StatusCodes.OK).json({
+                amount : amount,
+            });
+        }
+    }
+    catch(err){
+        console.error(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+});
 
 router.delete('/:purchaseId', isLoggedIn, async (req, res, next) => { // 필요없는 기능일 듯
 
