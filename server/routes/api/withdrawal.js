5e859d463c6b008260a3f9a52f93e14e519be4dc
trop100 <trop100@naver.com>
2021-10-24 08:04:39 +0000

    withdrawal 생성 api 수정

--- server/routes/api/withdrawal.js
+++ server/routes/api/withdrawal.js
@@ -3,9 +3,9 @@ var router = express.Router();
 const {StatusCodes} = require("http-status-codes");
 
 const { isLoggedIn, isAuthor } = require("../../middlewares/auth");
-const { sequelize,withdrawal, account , } = require("../../models");
+const { sequelize,withdrawal, account, author, purchase, book_detail, book,Sequelize: {Op} } = require("../../models");
 
-router.post('/', isLoggedIn, async(req, res, next) => {
+/*router.post('/', isLoggedIn, async(req, res, next) => {
     let amount = req.body.amount * 1;
     let author_id = req.user.id;
     const t = await sequelize.transaction();
@@ -31,18 +31,103 @@ router.post('/', isLoggedIn, async(req, res, next) => {
             request_withdrawal_amount : update_request_withdrawal_amount,
             amount_available_withdrawal : update_amount_available_withdrawal,
         },
-        {   
+        {
             where: {
                 author_id : author_id,
             },
             transaction : t,
 
         });
-        
+
         await t.commit();
         res.status(StatusCodes.CREATED).json({
             "withdrawal": newWithdrawal,
         });
+    }
+    catch(err){
+        await t.rollback();
+        console.error(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+});*/
+
+router.post('/', isLoggedIn, async(req, res, next) => {
+    let amount = parseInt(req.body.amount);
+    let author_id = req.user.id;
+    const t = await sequelize.transaction();
+
+    try{
+        const newWithdrawal = await withdrawal.create({
+            author_id : author_id,
+            amount: amount,
+            status: 1,
+        },
+        {
+            transaction : t,
+        });
+
+        const purchases = await purchase.findAll({
+            attributes: [
+                "id"
+            ],
+            where: {
+                status: 1,
+                remit_status: 0,
+            },
+            include: [
+                {
+                    model : book_detail,
+                    as : "book_detail",
+                    attributes : [],
+                    required: true,
+                    include : [
+                        {
+                            model: book,
+                            as : "book",
+                            where : {
+                                author_id: author_id
+                            },
+                            attributes: [],
+                        }
+                    ]
+                },
+            ],
+            transaction : t,
+        })
+
+        const purchaseUpdate = await purchase.update(
+            {
+                remit_status: 1,
+                withdrawal_id: newWithdrawal.id,
+            },
+            {
+                where: {
+                    id: {
+                        [Op.in]: purchases.map(p => p.id),
+                    }
+                },
+                transaction: t,
+            }
+        )
+
+        const authorUpdate = await author.update(
+            {
+                is_waiting: 1,
+            },
+            {
+                where:{
+                    member_id: author_id,
+                },
+                transaction: t,
+            }
+        )
+
+        if(authorUpdate && purchaseUpdate && newWithdrawal) {
+            await t.commit();
+            res.status(StatusCodes.CREATED).json({
+                "withdrawal": newWithdrawal,
+            });
+        }
     }
     catch(err){
         await t.rollback();
@@ -85,7 +170,7 @@ router.get('/', isLoggedIn, isAuthor,async (req, res, next) => {
     }
 
     // DB LOAD
-    
+
 });
 
 
