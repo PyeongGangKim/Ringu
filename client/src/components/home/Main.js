de693171c2d9b2b2ff7a6534f9ceb9eba81bd48b
trop100 <trop100@naver.com>
2021-12-02 05:21:46 +0000

    console 정리

--- client/src/components/home/Main.js
+++ client/src/components/home/Main.js
@@ -35,17 +35,16 @@ class Main extends Component {
         let latestBookParams = {
             is_approved: 1,
         }
-        
+
 
         try {
             const res = await API.sendGet(URL.api.book.list, params);
             const latestRes = await API.sendGet(URL.api.book.list, latestBookParams);
-            console.log(latestRes);
-            console.log(res.status);
+
             if(res.status === 200) state.bookList = res.data.bookList;
             if(latestRes.status === 200)state.latestBookList = latestRes.data.bookList;
             if(res.status === 200 || latestRes.status === 200) this.setState(state);
-            
+
         } catch(e) {
             console.error(e)
         }
