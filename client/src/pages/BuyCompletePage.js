cce5fdd20c909609179f781ee1b74b9bb72147b7
trop100 <yblim@idb.snu.ac.kr>
2022-02-21 01:45:28 +0900

    buy callback page 클래스형으로 변환

--- client/src/pages/BuyCompletePage.js
+++ client/src/pages/BuyCompletePage.js
@@ -8,7 +8,6 @@ import Footer from '../components/common/Footer';
 class BuyCompletePage extends Component {
     constructor(props) {
         super(props);
-        console.log(props)
     }
 
     render() {
