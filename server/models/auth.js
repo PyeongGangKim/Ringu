c5b2e8dcf334cd58b06e96c4bc5467d5a341cd7f
Ubuntu <ubuntu@ip-172-31-44-56.ap-northeast-2.compute.internal>
2022-02-21 13:47:16 +0000

    merge

--- server/models/auth.js
+++ server/models/auth.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
       }
     },
     token: {
-      type: DataTypes.STRING(24),
+      type: DataTypes.STRING(48),
       allowNull: false
     },
     ttl: {
