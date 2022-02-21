0a38354fc55b34efb735e596d70f81b451ffeaed
김평강 <21500185@handong.edu>
2022-02-21 22:19:03 +0900

    fix: reward_stat alias 변경

--- server/routes/admin/member.js
+++ server/routes/admin/member.js
@@ -41,7 +41,7 @@ router.get("/", async(req, res, next) => {
             include: [
                 {
                     model: reward_stat,
-                    as: "reward_stat",
+                    as: "reward_stats",
                     required: false,
                 },                
             ],
