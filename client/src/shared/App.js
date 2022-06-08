4525b96e3734bcf74e24a801ca9798313532549d
trop100 <yblim@idb.snu.ac.kr>
2022-06-08 17:43:38 +0900

    마이페이지 1페이지 통합 반영

--- client/src/shared/App.js
+++ client/src/shared/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Footer from '../components/common/Footer';
-import { Home, NotFound, MyPage, PasswordChangePage, NotificationChangePage, LeavePage, Purchases, Carts, FavBookPage, FavAuthorPage, AuthorRegisterDetailPage, AuthorPage, BookPage, RegisterPage, RegisterDetailPage, BuyPage, BuyCompletePage, BuyCallbackPage, SearchPage,NotificationPage} from '../pages';
+import { Home, NotFound, MyInfoPage, MyPage, PasswordChangePage, NotificationChangePage, LeavePage, PurchasePage, CartPage, FavBookPage, FavAuthorPage, AuthorRegisterDetailPage, AuthorPage, BookPage, RegisterPage, RegisterDetailPage, BuyPage, BuyCompletePage, BuyCallbackPage, SearchPage, NotificationPage} from '../pages';
 import { LoginPage, SignupPage, SignupDetailPage, SignupCallbackPage, WelcomePage, ModifyBookPage, ModifyBookSeriesPage, TermsPage, FindEmailPage, ChangePasswordPage, ChangePasswordCompletePage } from '../pages';
 
 import { ReviewPage, PaymentPage } from '../pages';
@@ -22,14 +22,7 @@ class App extends Component {
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/" component={Home} />{}
-                    <Route path="/mypage/password/update" component={PasswordChangePage} />{}
-                    <Route path="/mypage/notification/update" component={NotificationChangePage} />{}
                     <Route path="/mypage" component={MyPage} />{}
-                    <Route path="/leave" component={LeavePage} />{}
-                    <Route path="/purchases" component={Purchases} />{}
-                    <Route path="/carts" component={Carts} />{}
-                    <Route path="/favorite/book" component={FavBookPage} />{}
-                    <Route path="/favorite/author" component={FavAuthorPage} />{}
 
                     <Route path="/author/:author_id" component={AuthorPage} />{}
                     <Route path="/register/author/" component={AuthorRegisterDetailPage} />{}                    
