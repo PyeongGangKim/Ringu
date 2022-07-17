3959a69d6e2128a604f35087d0b39e564f8ffb32
trop100 <yblim@idb.snu.ac.kr>
2022-07-17 21:26:34 +0900

    minor fix

--- client/src/shared/App.js
+++ client/src/shared/App.js
@@ -18,42 +18,42 @@ class App extends Component {
     render() {
         return (
             <>
-            <GuideProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" component={Home} />{}
-                    <Route path="/mypage" component={MyPage} />{}
+                <GuideProvider>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route exact path="/" component={Home} />{}
+                            <Route path="/mypage" component={MyPage} />{}
 
-                    <Route path="/author/:author_id" component={AuthorPage} />{}
-                    <Route path="/register/author/" component={AuthorRegisterDetailPage} />{}                    
-                    <Route path="/payment" component={PaymentPage} />{}
-                    <Route path="/book/:book" component={BookPage} />
-                    
-                    <Route path="/register/book/:bookType" component={RegisterDetailPage} /> {}
-                    <Route path="/register/book" component={RegisterPage} /> {}
-                    <Route path="/modify/pub/:bookId" component={ModifyBookPage} /> {}
-                    <Route path="/modify/series/:bookId" component={ModifyBookSeriesPage} /> {}
-                    
-                    <Route path="/complete" component={BuyCompletePage} />{}
-                    <Route path="/buy/callback" component={BuyCallbackPage} />{}
-                    <Route path="/buy" component={BuyPage} />{}
-                    <Route path="/search" component={SearchPage} />{}
+                            <Route path="/author/:author_id" component={AuthorPage} />{}
+                            <Route path="/register/author/" component={AuthorRegisterDetailPage} />{}
+                            <Route path="/payment" component={PaymentPage} />{}
+                            <Route path="/book/:book" component={BookPage} />
+                            
+                            <Route path="/register/book/:bookType" component={RegisterDetailPage} /> {}
+                            <Route path="/register/book" component={RegisterPage} /> {}
+                            <Route path="/modify/pub/:bookId" component={ModifyBookPage} /> {}
+                            <Route path="/modify/series/:bookId" component={ModifyBookSeriesPage} /> {}
+                            
+                            <Route path="/complete" component={BuyCompletePage} />{}
+                            <Route path="/buy/callback" component={BuyCallbackPage} />{}
+                            <Route path="/buy" component={BuyPage} />{}
+                            <Route path="/search" component={SearchPage} />{}
 
-                    <Route exact path="/login/findpwd" component={FindEmailPage} />{}
-                    <Route exact path="/change_pwd/complete" component={ChangePasswordCompletePage} />{}
-                    <Route exact path="/change_pwd" component={ChangePasswordPage} />{}
-                    <Route exact path="/login" component={LoginPage} />{}
-                    <Route path="/signup/:sns/callback" component={SignupCallbackPage} />{}
-                    <Route path="/signup/step" component={SignupDetailPage} />{}
-                    <Route path="/signup" component={SignupPage} />{}
-                    <Route path="/welcome" component={WelcomePage} />{}
-                    <Route path="/review/:book_detail" component={ReviewPage} />{}
-                    <Route path="/notification" component={NotificationPage} />{}
-                    <Route path="/terms/:type" component={TermsPage} />{}
-                    <Route component={NotFound} />
-                </Switch>
-                <Footer/>
-                </BrowserRouter>
+                            <Route exact path="/login/findpwd" component={FindEmailPage} />{}
+                            <Route exact path="/change_pwd/complete" component={ChangePasswordCompletePage} />{}
+                            <Route exact path="/change_pwd" component={ChangePasswordPage} />{}
+                            <Route exact path="/login" component={LoginPage} />{}
+                            <Route path="/signup/:sns/callback" component={SignupCallbackPage} />{}
+                            <Route path="/signup/step" component={SignupDetailPage} />{}
+                            <Route path="/signup" component={SignupPage} />{}
+                            <Route path="/welcome" component={WelcomePage} />{}
+                            <Route path="/review/:book_detail" component={ReviewPage} />{}
+                            <Route path="/notification" component={NotificationPage} />{}
+                            <Route path="/terms/:type" component={TermsPage} />{}
+                            <Route component={NotFound} />
+                        </Switch>
+                        <Footer/>
+                    </BrowserRouter>
                 </GuideProvider>
             </>
         );
